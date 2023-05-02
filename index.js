ee18c8f019d6eea54ef34b7df486280e0e09db84
Todi Olubode <todimu18@gmail.com>
2023-05-02 15:55:56 +0100

    yo

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 require('dotenv').config();
 
 // Print out value of API key stored in .env file
-console.log(process.env.API_KEY)
+console.log(process.env.nldvceH1HQMsYL0ZBMbMxhTHKlvTyeV7)
+const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=dogs&limit=25&offset=0&rating=g&lang=en`
+
+async function getImage(query){
+    const response = await fetch("https://api.giphy.com/v1/gifs/search?api_key=nldvceH1HQMsYL0ZBMbMxhTHKlvTyeV7&q=dog&limit=25&offset=0&rating=g&lang=en")
+    const data = await response.json()
+}
